a393977966197568427323afe4c75b35360a8c83
Maninder Singh <manindersingh@Maninders-MacBook-Pro.local>
2019-03-27 16:12:52 -0400

    basic user schema defined, router.post works, password is hashed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,39 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const User = require('../../model/User');
 
 // @route GET api/users/test 
 // @desc tests post route 
 // @access private route
-router.get('/test', (req, res) => res.json( {msg:"user working"} ));
+router.get('/test', (req, res) => res.json({ msg: "user working" }));
+
+// @route get api/users/register
+// @desc register user
+// @access public
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: "Email Already Exists" });
+        } else {
+            const NewUser = new User({
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(NewUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    NewUser.password = hash;
+                    NewUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                })
+            })
+        }
+    })
+})
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 
-const app = express(); 
+//initialize a variable to express 
+const app = express();
 
+//body parser middleware 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//db configuration
 const db = require('./config/keys').mongoURI; 
 
+//conect to mongodb through mongoose
 mongoose
     .connect(db)
-    .then( () => console.log("mongoDB Connected"))
+    .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('i love pizza'));
+app.get('/', (req, res) => res.send('Mantis plagairism detection system'));
 
 const port = 3000; 
 
+//use routes 
 app.use('/api/users', users);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
